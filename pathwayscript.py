07e6429396138cfeafa6a8eb3e408bbcd7775554
Bruce <zsedcx22@hotmail.com>
2017-10-23 22:52:03 +0000

    searching directories.discovery

--- pathwayscript.py
+++ pathwayscript.py
@@ -5,7 +5,7 @@ lastFamily=""
 lastGenus=""
 looper=0
 sharks=[]
-x=open("CARCHARHINIFORMES.txt")
+x=open("SQUALIFORMES.txt")
 y=x.read()
 sharks=y.split(",")
 l=len(sharks)
@@ -13,14 +13,14 @@ l=len(sharks)
 while looper<l:
   if sharks[looper]=="Family":
     lastFamily=sharks[looper+1]
-    os.makedirs(os.path.dirname('sharks/CARCHARHINIFORMES/{0}/'.format(lastFamily)))
+    os.makedirs(os.path.dirname('sharks/SQUALIFORMES/{0}/'.format(lastFamily)))
     looper+=2
   elif sharks[looper]=="Genus":
     lastGenus=sharks[looper+1]
-    os.makedirs(os.path.dirname('sharks/CARCHARHINIFORMES/{0}/{1}/'.format(lastFamily,lastGenus)))
+    os.makedirs(os.path.dirname('sharks/SQUALIFORMES/{0}/{1}/'.format(lastFamily,lastGenus)))
     looper+=2
   else:
-    file=open('sharks/CARCHARHINIFORMES/{0}/{1}/{2}.txt'.format(lastFamily,lastGenus,sharks[looper]),'w')
+    file=open('sharks/SQUALIFORMES/{0}/{1}/{2}.txt'.format(lastFamily,lastGenus,sharks[looper]),'w')
     file.write('{0},{1}'.format(sharks[looper],sharks[looper+1]))
     looper+=2
 
--- sharkopedia.py
+++ sharkopedia.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 
 @app.route('/sharkopedia/')
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/sharkopedia/<sharkorder>/<sharkfamily>/')
 @app.route('/sharkopedia/<sharkorder>/<sharkfamily>/<sharkgenus>/')
 @app.route('/sharkopedia/<sharkorder>/<sharkfamily>/<sharkgenus>/<sharkname>')
+sharkie=[]
+for root, dirs, files in os.walk("sharks"):
+  for file in files:
+    if file.endswith(".txt"):
+      sharkie.append(file[:-4])
+
 def shark(sharkorder=None, sharkfamily=None, sharkgenus=None, sharkname=None):
   return render_template('sharktemplate.html', sharkorder=sharkorder,
   sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname)
