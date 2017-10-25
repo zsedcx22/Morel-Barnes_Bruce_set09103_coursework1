ca2bc47ff9c26b219ac6d3f89198fe3e296eee59
Bruce <zsedcx22@hotmail.com>
2017-10-25 01:19:50 +0000

    good jobbo

--- sharkopedia.py
+++ sharkopedia.py
@@ -1,21 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 app = Flask(__name__)
-
-@app.route('/sharkopedia/')
-@app.route('/sharkopedia/<sharkorder>/')
-@app.route('/sharkopedia/<sharkorder>/<sharkfamily>/')
-@app.route('/sharkopedia/<sharkorder>/<sharkfamily>/<sharkgenus>/')
+#xd
+@app.route('/sharkopedia/', methods=['POST','GET'])
+@app.route('/sharkopedia/<sharkorder>/', methods=['POST','GET'])
+@app.route('/sharkopedia/<sharkorder>/<sharkfamily>/', methods=['POST','GET'])
+@app.route('/sharkopedia/<sharkorder>/<sharkfamily>/<sharkgenus>/',
+methods=['POST','GET'])
 @app.route('/sharkopedia/<sharkorder>/<sharkfamily>/<sharkgenus>/<sharkname>')
-sharkie=[]
-for root, dirs, files in os.walk("sharks"):
-  for file in files:
-    if file.endswith(".txt"):
-      sharkie.append(file[:-4])
 
+#MODIFIERS FOR DIR ENDING UP SOMETHING LIKE directory+="/{0}".format(sharkorder)
 def shark(sharkorder=None, sharkfamily=None, sharkgenus=None, sharkname=None):
-  return render_template('sharktemplate.html', sharkorder=sharkorder,
-  sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname)
-
+  global directory
+  sharks=[]
+  finddir=(str(request.form.getlist('submit'))[3:])[:-2]
+  if request.method=='POST':
+    #print(request.form.getlist('submit')[1:])
+    if finddir=='all': #if the 'all' button has been pressed
+      for root, dirs, files in os.walk(directory): #for all directories and
+                                                   #files in the selected dir
+        for file in files: #for each file in the directory
+          if file.endswith(".txt"): #if it ends with '.txt' then
+            sharks.append(file) #remove the .txt and put the file name
+                                     #into a list
+            #print(file)
+      return render_template('sharktemplate.html', sharkorder=sharkorder,
+        sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname,
+        sharks=sharks)
+    elif finddir!=None:
+      directory+="/{0}".format(finddir)
+      if sharkgenus!=None: #if 'genus' has a value
+        for file in files: #for each file in the directory
+          if file.endswith(".txt"): #if it ends with '.txt' then
+            sharks.append(file[:-4]) #remove the .txt and put the file name
+                                     #into a list
+        return render_template('sharktemplate.html', sharkorder=sharkorder,
+          sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname,
+          sharks=sharks)
+      else:
+        sharks=os.listdir(directory)
+        return render_template('sharktemplate.html', sharkorder=sharkorder,
+          sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname,
+          sharks=sharks)
+  elif request.method=='GET':
+    sharks=os.listdir(directory)
+    return render_template('sharktemplate.html', sharkorder=sharkorder,
+      sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname,
+      sharks=sharks)
+directory="sharks"
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
